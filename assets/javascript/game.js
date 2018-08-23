bcaf7074ce37030d613ce4096857dea20faf7a80
snowember <timwestberg43@gmail.com>
2018-08-22 20:21:41 -0700

    inital html/css set up for page, cached the DOM as well

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+const start_button = document.getElementById("start")
+const wrongGuess_span = document.getElementById("wrong-g")
+const lettersUsed_p = document.getElementById("letters-sofar")
+const answer_p = document.getElementById("comp-ans")
+const losses_span = document.getElementById("loss-s")
+const wins_span = docuemnt.getElementById("win-s")
+const phrase_h5 = document.getElementById("phrase-opt")
+
